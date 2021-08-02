7347fdfd0d2892d2a34a8c2a53e0fc28b2c207c5
ATRS <82661604+ATRS7391@users.noreply.github.com>
2021-08-02 15:53:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,30 @@ window.addEventListener("load", () => {
       sounds[index].play();
     });
   });
+  document.addEventListener("keydown", function(event) {
+    if (event.key == 1){
+      sounds[0].currentTime = 0;
+      sounds[0].play();
+    }
+    else if (event.key == 2){
+      sounds[1].currentTime = 0;
+      sounds[1].play();
+    }
+    else if (event.key == 3){
+      sounds[2].currentTime = 0;
+      sounds[2].play();
+    }
+    else if (event.key == 4){
+      sounds[3].currentTime = 0;
+      sounds[3].play();
+    }
+    else if (event.key == 5){
+      sounds[4].currentTime = 0;
+      sounds[4].play();
+    }
+    else if (event.key == 6){
+      sounds[5].currentTime = 0;
+      sounds[5].play();
+    }
+  });
 });
